031467c3eeaac0dcf7f3cb5fd5b73671336f6aba
zainulabidin379 <ulabidinz1@gmail.com.com>
2024-01-17 08:16:44 +0500

    updated url path strategy

--- lib/extensions_kit.dart
+++ lib/extensions_kit.dart
@@ -13,6 +13,7 @@ export 'src/padding_ext.dart';
 export 'src/string_ext.dart';
 export 'src/text_ext.dart';
 export 'src/text_styles_ext.dart';
-export 'src/url_strategy.dart';
+export 'src/urlStrategy/url_strategy_non_web.dart'
+    if (dart.library.html) 'src/urlStrategy/url_strategy_web.dart';
 export 'src/widget_ext.dart';
 export 'src/gap_ext.dart';
--- lib/src/url_strategy.dart
+++
@@ -1,2 +0,0 @@
-export 'urlStrategy/url_strategy_non_web.dart'
-    if (dart.library.html) 'urlStrategy/url_strategy_web.dart';
